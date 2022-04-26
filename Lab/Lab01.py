0c3e4b35cc82287b8b947d102d4a75a762773374
Loudwhisper <55324106+loudwhisper402@users.noreply.github.com>
2022-04-26 18:01:31 -0400

    Create Lab01.py

---
+++ Lab/Lab01.py
@@ -0,0 +1,20 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+	product = 1
+	count = 0
+	while k != 0 and count < k:
+        product *= n
+        n -= 1
+        count += 1
+    return product
