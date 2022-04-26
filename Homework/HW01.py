e4b627b6ab873872a45e30c699be2f24a25b9ba1
Loudwhisper <55324106+loudwhisper402@users.noreply.github.com>
2022-04-26 01:00:26 -0400

    Update HW01.py

--- Homework/HW01.py
+++ Homework/HW01.py
@@ -127,3 +127,31 @@ def true_func():
 def false_func():
     "*** YOUR CODE HERE ***"
     print("61A")
+
+    
+    def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    "*** YOUR CODE HERE ***"
+    k = 1
+    while n != 1:
+        print(n)
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = n * 3 + 1
+        k += 1
+    print(n)
+    return k
