8cfb7276eddb53162404d5b3ec731b29e8c8e387
Loudwhisper <55324106+loudwhisper402@users.noreply.github.com>
2022-04-28 00:44:19 -0400

    Update Lab01.py

--- Lab/Lab01.py
+++ Lab/Lab01.py
@@ -47,3 +47,46 @@ def sum_digits(y):
         collect.append(a)
     
     return sum(collect)
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+
+
+# method 1: string
+
+    return "88" in str(n)
+
+# method 2: number
+
+    flag = 0
+    d = 0
+
+    while n > 0:
+
+        n, d = n // 10, n % 10
+
+        if d == 8 and flag == 1:
+            return True
+
+        elif d == 8:
+            flag = 1
+
+        else:
+            flag = 0
+
+    return False
