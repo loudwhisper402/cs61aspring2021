b93bfcb357707a0333fa6a0e093fb2b229b9a83d
Loudwhisper <55324106+loudwhisper402@users.noreply.github.com>
2022-05-03 00:02:24 -0400

    Create Hw02.py

---
+++ Homework/Hw02.py
@@ -0,0 +1,70 @@
+from operator import add, mul, sub
+
+square = lambda x: x * x
+
+identity = lambda x: x
+
+triple = lambda x: 3 * x
+
+increment = lambda x: x + 1
+
+
+HW_SOURCE_FILE = __file__
+
+
+def product(n, term):
+    """Return the product of the first n terms in a sequence.
+    n -- a positive integer
+    term -- a function that takes one argument to produce the term
+
+    >>> product(3, identity)  # 1 * 2 * 3
+    6
+    >>> product(5, identity)  # 1 * 2 * 3 * 4 * 5
+    120
+    >>> product(3, square)    # 1^2 * 2^2 * 3^2
+    36
+    >>> product(5, square)    # 1^2 * 2^2 * 3^2 * 4^2 * 5^2
+    14400
+    >>> product(3, increment) # (1+1) * (2+1) * (3+1)
+    24
+    >>> product(3, triple)    # 1*3 * 2*3 * 3*3
+    162
+    """
+    "*** YOUR CODE HERE ***"
+    result = 1
+    for i in range(1,n+1):
+        result *= term(i)
+    return result
+  
+def square(x):
+    return x * x
+
+def accumulate(combiner, base, n, term):
+    """Return the result of combining the first n terms in a sequence and base.
+    The terms to be combined are term(1), term(2), ..., term(n).  combiner is a
+    two-argument commutative function.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
+    72
+    >>> accumulate(lambda x, y: x + y + 1, 2, 3, square) # 
+    19
+    >>> accumulate(lambda x, y: 2 * (x + y), 2, 3, square)
+    58
+    >>> accumulate(lambda x, y: (x + y) % 17, 19, 20, square)
+    16
+    """
+    "*** YOUR CODE HERE ***"
+    result = base
+    for i in range(1,n+1):
+        result = combiner(result,term(i))
+    return result
+
+  
